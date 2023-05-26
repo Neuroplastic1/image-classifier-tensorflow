776e86a06f1a85c6b4b6e5ba688b769c855f3fa6
shafqat <64027495+Neuroplastic1@users.noreply.github.com>
2023-05-25 20:13:58 -0400

    function to load and preprocess images
    Defined the function to load and preprocess images

--- script.py
+++ script.py
@@ -9,3 +9,19 @@ model = tf.keras.Sequential([
                    output_shape=[1001],
                    trainable=False)
 ])
+
+# Define the function to load and preprocess images
+def load_and_prep_image(image_path, img_shape=224):
+    """
+    Reads an image from filename, turns it into a tensor and reshapes it
+    to img_shape (img_shape, img_shape, color_channels)
+    """
+    # Read in the image
+    img = tf.io.read_file(image_path)
+    # Decode the read file into a tensor
+    img = tf.image.decode_image(img)
+    # Resize the image  
+    img = tf.image.resize(img, size=[img_shape, img_shape])
+    # Rescale the image (get all values between 0 and 1)
+    img = img/255.
+    return img
